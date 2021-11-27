4846b9c6b80d6e41741da2cc42911734cdd6f3ea
ikhsanananda <ikhsanananda@apps.ipb.ac.id>
2021-11-27 20:02:08 +0700

    page buat parse data dari form predict

---
+++ backend.php
@@ -0,0 +1,18 @@
+<?php
+include('db.php');
+
+if(isset($_POST['send']))
+{
+    $diploma = $_POST['satu']; $sains = $_POST['dua'];
+    $sarjana = $_POST['tiga']; $ijazah = $_POST['empat'];
+    $transkrip = $_POST['lima']; $rekomendasi = $_POST['enam'];
+    $dosen = $_POST['tujuh']; $biaya = $_POST['delapan'];
+    $sinopsis = $_POST['sembilan']; $kelas = $_POST['sepuluh'];
+    $ipk = $_POST['ipk']; $tpa = $_POST['tpa'];
+    $doubledegree =$_POST['tigabelas']; $bahasa = $_POST['toefl'];
+    $penelitian = $_POST['limabelas']; $proposal = $_POST['enambelas'];
+
+    
+
+}
+?>
